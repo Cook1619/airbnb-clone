a969fd3361c2cc6a5cde83b7aaf49f679d19c68b
Mathew Cook <cook6348@gmail.com>
2021-09-10 11:52:19 -0500

    Adding get and delete routes

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,6 +1,10 @@
 import express from "express";
+import bodyParser from "body-parser";
+import { listings } from "./listings";
 const app = express();
 const port = 9000;
+
+app.use(bodyParser.json());
 
 app.get("/", (_req, res) => {
   res.send("Hello!");
@@ -8,6 +12,23 @@ app.get("/", (_req, res) => {
 
 app.get("/test", (req, res) => {
   res.send("test");
+});
+//listing route
+app.get("/listings", (_req, res) => {
+  return res.send(listings);
+});
+
+//delete listing
+app.post("/delete-listing", (req, res) => {
+  const id: string = req.body.id;
+
+  for (let i = 0; i < listings.length; i++) {
+    if (listings[i].id === id) {
+      return res.send(listings.splice(i, 1)[0]);
+    }
+  }
+
+  return res.send("failed to delete listing");
 });
 
 app.listen(port);
