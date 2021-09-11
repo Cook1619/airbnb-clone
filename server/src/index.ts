db1033284b252f95df528af289678c7d9d7cd661
Mathew Cook <cook6348@gmail.com>
2021-09-11 07:25:34 -0500

    gpl implementation

--- server/src/index.ts
+++ server/src/index.ts
@@ -24,6 +24,22 @@ app.post("/delete-listing", (req, res) => {
   return res.send("failed to delete listing");
 });
 
+// What GQL might look like when
+// Query: {
+//   listings: (obj, args, ctx) => {
+//     return listings;
+//   },
+// },
+// Mutation: {
+//   deleteListing: (obj, args, ctx) => {
+// 	  for (let i = 0; i < listings.length; i++) {
+// 	    if (listings[i].id === id) {
+// 	      return listings.splice(i, 1)[0];
+// 	    }
+// 	  }
+//   },
+// },
+
 app.listen(port);
 
 console.log(`[app]: running on port ${port}`);
